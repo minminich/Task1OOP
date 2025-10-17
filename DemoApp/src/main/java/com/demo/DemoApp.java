80c7ab9e765276c54544f40318d29dc2cc015d36
minminich <minminich@icloud.com>
2025-10-17 20:49:47 +0300

    created dependency

--- DemoApp/src/main/java/com/demo/DemoApp.java
+++ DemoApp/src/main/java/com/demo/DemoApp.java
@@ -1,10 +1,18 @@
 package com.demo;
 
+
+import com.lib.PasswordHasher;
+import com.lib.UserInput;
+
+import java.util.Scanner;
+
+
 /**
  * Hello world!
  */
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
     }
 }
